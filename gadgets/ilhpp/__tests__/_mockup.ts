797b0bfedb6cdb4661064dfe29aa625aed47e96b
diskdance <psnbaotg@protonmail.com>
2025-02-16 21:44:09 +0800

    Fix dev server errors

--- gadgets/ilhpp/__tests__/_mockup.ts
+++ gadgets/ilhpp/__tests__/_mockup.ts
@@ -1,5 +1,6 @@
 /// <reference types="requirejs" />
 import '../server/mockup_mw';
+import * as MobileStartup from '../server/mockup_mobile_startup';
 import * as HanAssist from 'hanassist';
 
 // Mockup mw.loader.using by redirecting to requirejs
@@ -22,7 +23,5 @@ define('ext.gadget.HanAssist', [], () => {
 });
 
 define('mobile.startup', () => {
-  return {
-    getOverlayManager() {},
-  };
+  return MobileStartup;
 });
