82a4ca6954dbef0304f2f8fd6da1663c43b87459
diskdance <psnbaotg@protonmail.com>
2025-03-02 14:45:56 +0800

    Move Playwright configs to root

--- gadgets/ilhpp/__tests__/_global_setup.ts
+++ gadgets/ilhpp/__tests__/_global_setup.ts
@@ -1,33 +1,32 @@
 import { build, mergeConfig } from 'vite';
 import config from '../vite.config';
 
-async function globalSetup() {
-  const mainConfig = config({ mode: 'testing', command: 'build' });
+// Fix cwd to Vite config's directory
+process.chdir(`${import.meta.dirname}/../`);
 
-  await build(
-    mergeConfig(mainConfig, {
-      build: {
-        emptyOutDir: true,
-        outDir: `${import.meta.dirname}/../../../dist/__tests_DO_NOT_USE__`,
-        lib: {
-          name: 'ext.gadget.ilhpp',
-          entry: `${import.meta.dirname}/../src/index.ts`,
-          formats: ['umd'],
-        },
-        rollupOptions: {
-          output: {
-            amd: {
-              id: 'ext.gadget.ilhpp',
-            },
-            globals: {
-              'ext.gadget.HanAssist': 'mw.libs.HanAssist',
-            },
+const mainConfig = config({ mode: 'testing', command: 'build' });
+
+await build(
+  mergeConfig(mainConfig, {
+    build: {
+      emptyOutDir: true,
+      outDir: `${import.meta.dirname}/../../../dist/__tests_DO_NOT_USE__`,
+      lib: {
+        name: 'ext.gadget.ilhpp',
+        entry: `${import.meta.dirname}/../src/index.ts`,
+        formats: ['umd'],
+      },
+      rollupOptions: {
+        output: {
+          amd: {
+            id: 'ext.gadget.ilhpp',
+          },
+          globals: {
+            'ext.gadget.HanAssist': 'mw.libs.HanAssist',
           },
         },
       },
-      logLevel: 'error',
-    }),
-  );
-}
-
-export default globalSetup;
+    },
+    logLevel: 'error',
+  }),
+);
--- playwright.config.js
+++ playwright.config.js
@@ -5,13 +5,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './__tests__',
   fullyParallel: true,
   forbidOnly: !!process.env.CI, // Fail the build on CI if accidentally left test.only in the source code.
   retries: process.env.CI ? 2 : undefined, // Opt out of retries on CI
   workers: process.env.CI ? 1 : undefined, // Opt out of parallel tests on CI
   reporter: 'html',
-  globalSetup: './__tests__/_global_setup',
 
   // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
   use: {
@@ -24,14 +22,28 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] },
-      grep: /(links_|popups_desktop_)/,
+      name: 'Setup ilhpp',
+      testDir: './gadgets/ilhpp/__tests__',
+      testMatch: /_global_setup\.ts/,
     },
     {
-      name: 'Mobile Chrome',
+      name: 'Setup ilhpp-settings',
+      testDir: './gadgets/ilhpp-settings/__tests__',
+      testMatch: /_global_setup\.ts/,
+    },
+    {
+      name: 'Chromium ilhpp',
+      testDir: './gadgets/ilhpp/__tests__',
+      use: { ...devices['Desktop Chrome'] },
+      grep: /(links_|popups_desktop_)/,
+      dependencies: ['Setup ilhpp'],
+    },
+    {
+      name: 'Mobile Chrome ilhpp',
+      testDir: './gadgets/ilhpp/__tests__',
       use: { ...devices['Pixel 7'] },
       grep: /popups_mobile_/,
+      dependencies: ['Setup ilhpp'],
     },
   ],
 });
